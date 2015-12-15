aa5f7e0e851cd14e47a86b8ee7571e46262df4e2
Adam Moelis <adam.moelis@gmail.com>
2015-12-15 13:07:38 -0500

    supports postgres instead of sqlite

--- quiz_master/quiz_master/settings.py
+++ quiz_master/quiz_master/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'quiz_master.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'quizdb',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
 
