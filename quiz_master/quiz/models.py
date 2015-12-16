c3292c319ea89cabf0beea88b5de227a4e5d75d4
Adam Moelis <adam.moelis@gmail.com>
2015-12-16 11:14:31 -0500

    player and team models added

--- quiz_master/quiz/models.py
+++ quiz_master/quiz/models.py
@@ -5,16 +5,18 @@ from django.db import models
 # Create your models here.
 
 class Team(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True) # set when it's created
     updated_at = models.DateTimeField(auto_now=True) # set every time it's updated
 
 class Player(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    position = models.CharField(max_length=30) # QB, WR, RB. LT, LG, C, RG, RT, DE, DT, OLB, MLB, SS, FS, CB
-    unit = models.CharField(max_length=30) # offense or defense
+    position = models.CharField(max_length=10) # QB, WR, RB. LT, LG, C, RG, RT, DE, DT, OLB, MLB, SS, FS, CB
+    unit = models.CharField(max_length=20) # offense or defense
+    number = models.IntegerField()
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     is_being_tested = models.BooleanField() # true if player has been assigned a test
+    image_url = models.ImageField()
     created_at = models.DateTimeField(auto_now_add=True) # set when it's created
     updated_at = models.DateTimeField(auto_now=True) # set every time it's updated
--- quiz_master/quiz_master/settings.py
+++ quiz_master/quiz_master/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'formation',
+    'quiz.apps.QuizConfig'
 )
 
 MIDDLEWARE_CLASSES = (
