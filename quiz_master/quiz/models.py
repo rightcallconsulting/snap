9fa9a941bc1b9f77457419837667763ace7955d4
Adam Moelis <adam.moelis@gmail.com>
2015-12-16 11:59:08 -0500

    add formation and test models

--- quiz_master/quiz/models.py
+++ quiz_master/quiz/models.py
@@ -25,8 +25,26 @@ class Player(models.Model):
 class Play(models.Model):
     name = models.CharField(max_length=100)
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
-    players = models.ManyToManyField(Player)
     formation = models.ForeignKey(Formation, on_delete=models.CASCADE)
+    players = models.ManyToManyField(Player)
+    tests = models.ManyToManyField(Test)
+    formation = models.ForeignKey(Formation, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True) # set when it's created
+    updated_at = models.DateTimeField(auto_now=True) # set every time it's updated
 
 class Formation(models.Model):
     name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True) # set when it's created
+    updated_at = models.DateTimeField(auto_now=True) # set every time it's updated
+
+class Test(models.Model):
+    type_of_test = models.CharField(max_length=100)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    score = models.IntegerField()
+    skips = models.IntegerField()
+    incorrect_guesses = models.IntegerField()
+    deadline = models.DateTimeField()
+    completed = models.BooleanField()
+    in_progress = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True) # set when it's created
+    updated_at = models.DateTimeField(auto_now=True) # set every time it's updated
