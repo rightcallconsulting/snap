ef560ff671fe3e558fbe5ccda03a59bda1a60cfd
Adam Moelis <adam.moelis@gmail.com>
2015-12-16 14:57:39 -0500

    add a dashboard app

--- quiz_master/quiz_master/urls.py
+++ quiz_master/quiz_master/urls.py
@@ -18,4 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^quiz/', include('quiz.urls')),
+    url(r'', include('dashboard.urls')),
+
 ]
