6e6d5d88f56f8b3de905332e4317e3658dfd786e
Adam Moelis <adam.moelis@gmail.com>
2015-12-16 16:53:47 -0500

    display a list of players view

--- quiz_master/quiz/views.py
+++ quiz_master/quiz/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 # Create your views here.
 
+from .models import Player, Team, Play, Formation
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the home page")
+    player_list = Player.objects.all()
+    context = {'player_list': player_list}
+    return render(request, 'quiz/index.html', context)
+
+def players(request):
+    player_list = Player.objects.all()
+    output = ', '.join([p.full_name() for p in player_list])
+    return HttpResponse(output)
