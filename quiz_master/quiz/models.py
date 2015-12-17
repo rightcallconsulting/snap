23c8e82dd6f90b3757a52200cf63735319b2c3c2
Adam Moelis <adam.moelis@gmail.com>
2015-12-17 18:30:56 -0500

    save before integrating theme

--- quiz_master/quiz/models.py
+++ quiz_master/quiz/models.py
@@ -15,7 +15,7 @@ class Team(models.Model):
 class Player(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    position = models.CharField(max_length=10) # QB, WR, RB. LT, LG, C, RG, RT, DE, DT, OLB, MLB, SS, FS, CB
+    position = models.CharField(max_length=10) # QB, WR, RB, LT, LG, C, RG, RT, DE, DT, OLB, MLB, SS, FS, CB
     unit = models.CharField(max_length=20) # offense or defense
     number = models.IntegerField()
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
--- quiz_master/quiz_master/settings.py
+++ quiz_master/quiz_master/settings.py
@@ -111,7 +111,6 @@ import dj_database_url
 
 DATABASES['default'] =  dj_database_url.parse('postgres://localhost:5432/quizdb')
 
-
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
