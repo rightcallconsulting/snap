53b8cb7c3518317aaa6e709ab79a25d4e54054bf
dylan <dylan@rightcallconsulting.com>
2016-01-10 13:15:01 -0500

    added configuration vars for aws s3 database

--- quiz_master/settings.py
+++ quiz_master/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 
-DATABASES['default'] =  dj_database_url.parse('postgres://localhost:5432/quizdb')
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
