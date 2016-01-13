8da60fcc20c3b6e7bf8d4fee84367b282b568847
Adam Moelis <adam.moelis@gmail.com>
2016-01-13 16:06:08 -0700

    add custom fields to the form

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,5 +1,24 @@
 from __future__ import unicode_literals
-
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.db import models
 
 # Create your models here.
+
+class UserCreateForm(UserCreationForm):
+    username = forms.CharField(required=True, widget=forms.TextInput(attrs={'class' : 'form-control input-sm bounceIn animation-delay2"'}))
+    email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'class' : 'form-control input-sm bounceIn animation-delay2"'}))
+    password1 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'class' : 'form-control input-sm bounceIn animation-delay2"'}))
+    password2 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'class' : 'form-control input-sm bounceIn animation-delay2"'}))
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(UserCreateForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import RequestContext, loader
+from django.contrib.auth import logout, authenticate, login
 
 from quiz.models import Player, Team, Play, Formation
+from dashboard.models import UserCreateForm
 from IPython import embed
 
 # Create your views here.
@@ -11,10 +16,45 @@ def homepage(request):
     return render(request, 'dashboard/homepage.html')
 
 def login(request):
+    embed()
     return render(request, 'dashboard/login.html')
 
+# def login_user(request):
+#     embed()
+#     # username = request.POST['username']
+#     # password = request.POST['password']
+#     # user = authenticate(username=username, password=password)
+#     # if user is not None:
+#     #     if user.is_active:
+#     #         login(request, user)
+#     #         # Redirect to a success page.
+#     #     else:
+#     #         # Return a 'disabled account' error message
+#     # else:
+#     #     # Return an 'invalid login' error message.
+#     return render(request, 'dashboard/login.html')
+#
+# def logout_user(request):
+#     # logout(request)
+#     # Redirect to a success page
+#     return render(request, 'dashboard/login.html')
+
+
 def register(request):
-    return render(request, 'dashboard/register.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            return HttpResponseRedirect("/books/")
+        # user = User.objects.create_user(username=request.POST['username'],
+        # password=request.POST['password'], email=request.POST['email'])
+        # return HttpResponseRedirect("/")
+    else:
+        form = UserCreateForm()
+    return render(request, 'dashboard/register.html', {
+        'form': form,
+    })
+    # return render(request, 'dashboard/register.html')
 
 def timeline(request):
     return render(request, 'dashboard/timeline.html')
