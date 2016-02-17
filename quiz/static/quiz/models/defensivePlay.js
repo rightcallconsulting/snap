d0ef59a9ad5d3cdc5312a965c1194cf7d8a79bb5
Adam Moelis <adam.moelis@gmail.com>
2016-02-17 11:59:19 -0500

    fix name of defense play

--- quiz/static/quiz/models/defensivePlay.js
+++ quiz/static/quiz/models/defensivePlay.js
@@ -128,45 +128,8 @@ DefensivePlay.prototype.establishOffensiveFormationFromArray = function(arrayOfO
 };
 
 DefensivePlay.prototype.establishOffenseToDraw = function(){
-  var offensiveFormationID = this.offensiveFormationID;
-  var newFormation;
-  var formation = this;
-  $.getJSON('/quiz/teams/1/formations/'+ this.offensiveFormationID, function(formationObject, jqXHR){
-    formationObject[0].fields.id = formationObject.pk;
-    formationObject[0].fields.positions = [];
-    newFormation = new Formation(formationObject[0].fields);
-    newFormation.playName = formationObject[0].fields.name;
-    $.getJSON('/quiz/teams/1/formations/'+ offensiveFormationID +'/positions', function(data, jqXHR){
-      data.forEach(function(position){
-        position.fields.id = position.pk;
-        position.fields.x = position.fields.startX;
-        position.fields.y = position.fields.startY;
-        position.fields.pos = position.fields.name;
-        position.fields.num = position.fields.pos;
-        position.fields.gapYPoint = position.fields.gapYardY;
-        position.fields.gapXPoint = position.fields.gapYardX;
-        position.fields.zoneYPoint = position.fields.zoneYardY;
-        position.fields.zoneXPoint = position.fields.zoneYardX;
-        var newPlayer = new Player(position.fields)
-        if(newPlayer.pos==="QB"){
-          newPlayer.fill = color(212, 130, 130);
-        }
-        else if(newPlayer.pos==="OL" || newPlayer.pos ==="LT" || newPlayer.pos ==="LG" || newPlayer.pos ==="C" || newPlayer.pos ==="RG" || newPlayer.pos ==="RT"){
-          newPlayer.fill = color(143, 29, 29);
-        }
-        else{
-          newPlayer.fill = color(255, 0, 0);
-        }
-        newFormation.positions.push(newPlayer);
-      })
-      newFormation.populatePositions();
-      formation.offensiveFormationObject = newFormation;
 
-    })
-  })
-}
-
-
+};
 
 DefensivePlay.prototype.drawAllPlayersWithOffense = function(field){
     this.defensivePlayers.forEach(function(player){
