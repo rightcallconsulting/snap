5d6726737b2047a37439eb25c380191d0f028dd4
Will <will@rightcallconsulting.com>
2016-03-13 22:51:35 -0600

    Added zones

--- quiz/static/quiz/models/zone.js
+++ quiz/static/quiz/models/zone.js
@@ -1,14 +1,3 @@
-//// CREATING FIELD ZONES FOR PASS COVERAGE ////
-
-/*
-flatWidth = 5.333
-hookWidth = 10.666
-
-flatDepth = 6
-hookDepth = 12 
-*/
-
-
 Field.prototype.getOuts = function(ballX, ballY, strength){
   if(strength === 0){
     var xCoord = ballX - 26.65;
@@ -19,6 +8,7 @@ Field.prototype.getOuts = function(ballX, ballY, strength){
   }
   return [xCoord, yCoord, 5.333, 5];
 };
+
 Field.prototype.getFlats = function(ballX, ballY, strength){
   if(strength === 0){
     var xCoord = ballX - 26.65;
@@ -29,6 +19,7 @@ Field.prototype.getFlats = function(ballX, ballY, strength){
   }
   return [xCoord, yCoord, 5.333, 5];
 };
+
 Field.prototype.getCurls = function(ballX, ballY, strength){
   if(strength === 0){
     var xCoord = ballX - 21.333;
@@ -39,6 +30,7 @@ Field.prototype.getCurls = function(ballX, ballY, strength){
   }
   return [xCoord, yCoord, 5.333, 5];
 };
+
 Field.prototype.getStops = function(ballX, ballY, strength){
   if(strength === 0){
     var xCoord = ballX - 21.333;
@@ -49,7 +41,8 @@ Field.prototype.getStops = function(ballX, ballY, strength){
   }
   return [xCoord, yCoord, 5.333, 5];
 };
-Field.prototype.getHook = function(ballX, ballY, strength){
+
+Field.prototype.getHooks = function(ballX, ballY, strength){
   if(strength === 0){
     // STRONG HOOK ZONE
     var xCoord = ballX - 16;
@@ -76,74 +69,72 @@ Field.prototype.getPixelZone = function(zone){
   pixelZone[3] = this.yardsToPixels(zone[3]);
   return pixelZone;
 };
-/*
+
 Field.prototype.drawFlats = function(){
   var ballX = test.getCurrentFormation().oline[2].x;
   var ballY = test.getCurrentFormation().oline[2].y;
-  var leftFlat; 
-  var rightFlat;
-  var a = leftFlat === field.getPixelZone(field.getFlat(ballX, ballY, 0));
-  var b = rightFlat === field.getPixelZone(field.getFlat(ballX, ballY, 1));
-  
-  fill(220, 220, 0);
-  rect(a[0], a[1], a[2], a[3]);
-  rect(b[0], b[1], b[2], b[3]);
-};
-
-Field.prototype.drawHooks = function(){
-  var ballX = test.getCurrentFormation().oline[2].x;
-  var ballY = test.getCurrentFormation().oline[2].y;
-  var leftHook;
-  var rightHook;
-  var a = leftHook === field.getPixelZone(field.getHook(ballX, ballY, 0));
-  var b = rightHook === field.getPixelZone(field.getHook(ballX, ballY, 1));
-
-  rect(a[0], a[1], a[2], a[3]);
-  rect(b[0], b[1], b[2], b[3]);
-};
-*/
-/// 3 DEEP ZONE ///
-Field.prototype.drawAllCoverageZones = function(){
-  stroke(0);
-  var ballX = test.getCurrentFormation().oline[2].x;
-  var ballY = test.getCurrentFormation().oline[2].y;
-  /// HOLE ///
-  var hole = this.getPixelZone(this.getHole(ballX, ballY));
-  fill(108, 190, 130);
-  rect(hole[0], hole[1], hole[2], hole[3]);
-
-  /// FLATS ///
   var leftFlat = this.getPixelZone(this.getFlats(ballX, ballY, 0));
   var rightFlat = this.getPixelZone(this.getFlats(ballX, ballY, 1));
   fill(220, 220, 0);
   rect(leftFlat[0], leftFlat[1], leftFlat[2], leftFlat[3]);
   rect(rightFlat[0], rightFlat[1], rightFlat[2], rightFlat[3]);
-  
-  /// HOOKS ///
-  var leftHook = field.getPixelZone(this.getHook(ballX, ballY, 0));
-  var rightHook = field.getPixelZone(this.getHook(ballX, ballY, 1));
+};
+
+Field.prototype.drawHooks = function(){
+  var ballX = test.getCurrentFormation().oline[2].x;
+  var ballY = test.getCurrentFormation().oline[2].y;
+  var leftHook = this.getPixelZone(this.getHooks(ballX, ballY, 0));
+  var rightHook = this.getPixelZone(this.getHooks(ballX, ballY, 1));
   fill(255, 100, 135);
   rect(leftHook[0], leftHook[1], leftHook[2], leftHook[3]);
   rect(rightHook[0], rightHook[1], rightHook[2], rightHook[3]);
+};
 
-    /// OUTS ///
-    var leftOut = field.getPixelZone(this.getOuts(ballX, ballY, 0));
-    var rightOut = field.getPixelZone(this.getOuts(ballX, ballY, 1));
-    fill(220, 80, 230);
-    rect(leftOut[0], leftOut[1], leftOut[2], leftOut[3]);
-    rect(rightOut[0], rightOut[1], rightOut[2], rightOut[3]); 
+Field.prototype.drawOuts = function(){
+  var ballX = test.getCurrentFormation().oline[2].x;
+  var ballY = test.getCurrentFormation().oline[2].y;
+  var leftOut = this.getPixelZone(this.getOuts(ballX, ballY, 0));
+  var rightOut = this.getPixelZone(this.getOuts(ballX, ballY, 1));
+  fill(220, 80, 230);
+  rect(leftOut[0], leftOut[1], leftOut[2], leftOut[3]);
+  rect(rightOut[0], rightOut[1], rightOut[2], rightOut[3]);
+};
 
-    /// STOPS ///
-    var leftStop = this.getPixelZone(this.getStops(ballX, ballY, 0));
-    var rightStop = this.getPixelZone(this.getStops(ballX, ballY, 1));
-    fill(242, 164, 80);
-    rect(leftStop[0], leftStop[1], leftStop[2], leftStop[3]);
-    rect(rightStop[0], rightStop[1], rightStop[2], rightStop[3]);
+Field.prototype.drawStops = function(){
+  var ballX = test.getCurrentFormation().oline[2].x;
+  var ballY = test.getCurrentFormation().oline[2].y;
+  var leftStop = this.getPixelZone(this.getStops(ballX, ballY, 0));
+  var rightStop = this.getPixelZone(this.getStops(ballX, ballY, 1));
+  fill(220, 80, 230);
+  rect(leftStop[0], leftStop[1], leftStop[2], leftStop[3]);
+  rect(rightStop[0], rightStop[1], rightStop[2], rightStop[3]);
+};
 
-    /// CURLS ///
-    var leftCurl = this.getPixelZone(this.getCurls(ballX, ballY, 0));
-    var rightCurl = this.getPixelZone(this.getCurls(ballX, ballY, 1));
-    fill(180, 80, 240);
-    rect(leftCurl[0], leftCurl[1], leftCurl[2], leftCurl[3]);
-    rect(rightCurl[0], rightCurl[1], rightCurl[2], rightCurl[3]);    
-  };
+Field.prototype.drawCurls = function(){
+  var ballX = test.getCurrentFormation().oline[2].x;
+  var ballY = test.getCurrentFormation().oline[2].y;
+  var leftCurl = this.getPixelZone(this.getCurls(ballX, ballY, 0));
+  var rightCurl = this.getPixelZone(this.getCurls(ballX, ballY, 1));
+  fill(180, 80, 240);
+  rect(leftCurl[0], leftCurl[1], leftCurl[2], leftCurl[3]);
+  rect(rightCurl[0], rightCurl[1], rightCurl[2], rightCurl[3]);
+};
+
+Field.prototype.drawHole = function(){
+  var ballX = test.getCurrentFormation().oline[2].x;
+  var ballY = test.getCurrentFormation().oline[2].y;
+  var hole = this.getPixelZone(this.getHole(ballX, ballY));
+  fill(108, 190, 130);
+  rect(hole[0], hole[1], hole[2], hole[3]);
+};
+
+/// 3 DEEP ZONE ///
+Field.prototype.drawAllCoverageZones = function(){
+  stroke(0);
+  this.drawFlats();
+  this.drawHooks();
+  this.drawOuts();
+  this.drawStops();
+  this.drawCurls();
+  this.drawHole();
+};
