1443093da7b0cb607ab76c699e84e0d1cffab607
Nick Gilligan <nhgilligan@gmail.com>
2016-04-18 17:07:26 -0400

    Add player custom quiz form

---
+++ quiz/utils/CustomQuizOrders.py
@@ -0,0 +1,10 @@
+CUSTOM_QUIZ_ORDERS = [
+    {
+        'text': 'Recently added',
+        'param_value': 'recent',
+    },
+    {
+        'text': 'My worst performers',
+        'param_value': 'worst',
+    },
+]
