4f434ecbf1b55d4cba939f1b40375dae5cf81427
Sam Fisher <sam@rightcallconsulting.com>
2016-06-06 10:09:02 -0600

    More cleanup of create play

--- dashboard/static/dashboard/js/edit_test.js
+++ dashboard/static/dashboard/js/edit_test.js
@@ -132,6 +132,9 @@ function draw() {
         textSize(14);
         textAlign(CENTER, CENTER);
         text(this.num, x, y);
+        if(this.blockingAssignmentObject){
+          debugger;
+        }
       }
       else {
         noStroke();
