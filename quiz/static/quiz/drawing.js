583addc3eb521c5efeb36414194a9a691d07fbaa
dylan <dylan@rightcallconsulting.com>
2016-08-24 14:25:32 -0600

    Dotted line function is almost perfect.

--- quiz/static/quiz/drawing.js
+++ quiz/static/quiz/drawing.js
@@ -14,19 +14,29 @@ function dottedLine(x1, y1, x2, y2) {
 	var deltaY = y2 - y1;
 	var dist = sqrt(pow(deltaX, 2) + pow(deltaY, 2));
 	var alpha = atan(deltaY/deltaX);
-	var stepSizePercentage = 0.05;
-	var segmentDist = dist*stepSizePercentage;
+	var segmentDist = 5;
+	var stepSizePercentage = segmentDist/dist;
 
 	var xDiff = cos(alpha)*segmentDist;
 	var yDiff = sin(alpha)*segmentDist;
+
+	if (deltaX <= 0) {
+		xDiff = -xDiff;
+		yDiff = -yDiff;
+	}
 
 	var xStart = x1;
 	var yStart = y1;
 	var xEnd = x1+xDiff;
 	var yEnd = y1+yDiff;
 
-	for (var i = 0; i <= 1/stepSizePercentage; ++i) {
+	for (var i = 0; i < 1/stepSizePercentage; ++i) {
 		if (i%2 == 0) {
+			var j = i+1;
+			if (j >= 1/stepSizePercentage) {
+				xEnd = x2;
+				yEnd = y2;
+			}
 			line(xStart, yStart, xEnd, yEnd);
 		}
 
