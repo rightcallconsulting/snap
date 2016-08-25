d960aca58fcb2a0663016cae81e898d65ad1cf99
dylan <dylan@rightcallconsulting.com>
2016-08-25 12:59:55 -0600

    Fixed deltaX bug.

--- quiz/static/quiz/models/player.js
+++ quiz/static/quiz/models/player.js
@@ -484,14 +484,9 @@ Player.prototype.drawKickOutRight = function(field, currentX, currentY) {
 	var x1 = currentX;
 	var y1 = currentY;
 	var x2, y2;
-
-	if (deltaX >= 0) {
-		x2 = currentX + xDiff;
-		y2 = currentY + yDiff;
-	} else {
-		x2 = currentX - xDiff;
-		y2 = currentY - yDiff;
-	}
+	
+	x2 = currentX + xDiff;
+	y2 = currentY + yDiff;
 
 	x1 = field.getTranslatedX(x1);
 	y1 = field.getTranslatedY(y1);
@@ -537,13 +532,8 @@ Player.prototype.drawKickOutLeft = function(field, currentX, currentY) {
 	var y1 = currentY;
 	var x2, y2;
 
-	if (deltaX >= 0) {
-		x2 = currentX + xDiff;
-		y2 = currentY + yDiff;
-	} else {
-		x2 = currentX - xDiff;
-		y2 = currentY - yDiff;
-	}
+	x2 = currentX + xDiff;
+	y2 = currentY + yDiff;
 
 	x1 = field.getTranslatedX(x1);
 	y1 = field.getTranslatedY(y1);
