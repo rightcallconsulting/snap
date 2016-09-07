becf3dfcf231d388cddea5ab11d44781527c6279
dylan <dylan@rightcallconsulting.com>
2016-09-07 14:25:49 -0600

    create blockType object

---
+++ quiz/static/quiz/models/blockType.js
@@ -0,0 +1,21 @@
+
+//***************************************************************************//
+//																			 //
+// blockType.js - Right Call Consulting. All Rights Reserved. 2016			 //
+//																			 //
+//***************************************************************************//
+//																			 //
+// A block type represents some offensive players (for now) block.            //
+//																			 //
+//***************************************************************************//
+
+var BlockType = function(config) {
+	
+};
+
+//***************************************************************************//
+//***************************************************************************//
+
+// draw determines the type of the block and calls the appropriate draw
+// function for that type of block.
+BlockType.prototype.draw = function() {};
