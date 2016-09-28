4e48fcd46bc20e07e3f532e7608ab7336924a1e2
dylan <dylan@rightcallconsulting.com>
2016-09-28 17:58:54 -0600

    Admin site changes.

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 from .models import Coach, PlayerGroup, Concept, Quiz, QuestionAttempted
 from quiz.models import Player
 
+admin.site.site_header = "Right Call Consulting administration"
+admin.site.site_title = "Snap site admin"
+
 # Define an inline admin descriptor for Coach model
 # which acts a bit like a singleton
 class CoachInline(admin.StackedInline):
