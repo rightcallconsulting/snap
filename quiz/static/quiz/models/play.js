66f9c3e82a9b514b8d8a6d4b3344654071a0316d
Sam Fisher <sam@rightcallconsulting.com>
2016-11-15 13:17:59 -0700

    Workaround for man blocks getting deleted when draggind defensive players

--- quiz/static/quiz/models/play.js
+++ quiz/static/quiz/models/play.js
@@ -575,9 +575,12 @@ function createPlayFromJson(playJsonDictionary) {
 
 			if (primaryAssignment.type === 1) {
 				for (k in defensivePlayersArray) {
-					if (primaryAssignment.player.x === defensivePlayersArray[k].x && primaryAssignment.player.y === defensivePlayersArray[k].y) {
-						playerToBlock = defensivePlayersArray[k];
+					if (primaryAssignment.player.pos === defensivePlayersArray[k].pos){
+						if (playerToBlock == null || (primaryAssignment.player.x === defensivePlayersArray[k].x && primaryAssignment.player.y === defensivePlayersArray[k].y)) {
+							playerToBlock = defensivePlayersArray[k];
+						}
 					}
+
 				}
 			}
 
