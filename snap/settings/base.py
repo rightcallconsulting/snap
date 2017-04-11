c03bd2a2ae4791458de2378f685ce3989071ba5e
dwesty17 <j.dylan.westerhold@gmail.com>
2017-04-10 22:09:54 -0700

    Django database cleanup work

--- snap/settings/base.py
+++ snap/settings/base.py
@@ -101,18 +101,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'snap.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'snapdb',
-        'PORT': '5432'
-    }
-}
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
